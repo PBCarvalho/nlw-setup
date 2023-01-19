fbd3baf5c1669cd9ad03f97f29441456e84518aa
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-01-18 21:45:26 -0300

    Projeto NLWSetup

---
+++ nlw-setup/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const nlwSetup = new NLWSetup(form);
+
+const data = {
+    run: ['01-01', '01-02', '01-03', '01-06', '01-07', '01-08', '01-07', '01-08', '01-09', '01-10'],
+    journal: ['01-03'],
+    exercice: ['01-02']
+}
+
+nlwSetup.setData(data);
+nlwSetup.load();
