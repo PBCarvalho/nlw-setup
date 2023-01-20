a473c91cb0d0a8c2357d472acb9f683c4f4ef614
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-01-20 12:50:21 -0300

    Projeto NLWSetup

--- nlw-setup/script.js
+++ nlw-setup/script.js
@@ -1,11 +1,27 @@
-const form = document.querySelector('form');
-const nlwSetup = new NLWSetup(form);
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-    run: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07', '01-08', '01-09', '01-10'],
-    journal: ['01-03'],
-    exercice: ['01-02']
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data);
-nlwSetup.load();
+function save() {
+  localStorage.setItem("NLWSetup@habitis", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habitis")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
